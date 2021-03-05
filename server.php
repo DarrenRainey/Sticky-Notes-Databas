4b8214877fa0e670ce8800b27511c67649e67fbd
Darren Rainey <darren@darrenraineys.co.uk>
2021-03-05 22:28:37 +0000

    Created addNote function: WARNING INSECURE code
    this was quickly thrown together and likely will have sql injection vulnerabilities DO NOT use in production or untrusted environments.

--- server.php
+++ server.php
@@ -4,9 +4,13 @@ $db = new SQLite3('notes.db');
 $results = $db->query('SELECT notes FROM notes');
 $allNotes = array();;
 while ($row = $results->fetchArray()) {
- //   var_dump($row);
 	$allNotes[] = $row;
 }
-
-print json_encode($allNotes);
+if( isset($_GET['addNote']) == true){
+$query = 'INSERT INTO notes VALUES (\'' .$_GET['addNote'] . '\')';
+$addDB = $db->query($query);
+	echo "OK";
+} else{
+	print json_encode($allNotes);
+}
 ?>
